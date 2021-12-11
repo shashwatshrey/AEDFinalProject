ea97f67b63b8a75b30d3109ed7d1e9c66096d4d0
Vraj Swetal Mashruwala <mashruwala.v@northeastern.edu>
2021-12-11 01:19:48 -0500

    merge commit

--- AEDFinalProject/src/ui/DistributionRole/DistributionWorkAreaJPanel.java
+++ AEDFinalProject/src/ui/DistributionRole/DistributionWorkAreaJPanel.java
@@ -64,10 +64,10 @@ public class DistributionWorkAreaJPanel extends javax.swing.JPanel {
         jLabel1 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
-        VaccinationjDateChooser = new com.toedter.calendar.JDateChooser();
         btnSchedule = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
         txtvaccineCount = new javax.swing.JTextField();
+        VaccinationjDateChooser = new com.toedter.calendar.JDateChooser();
 
         jLabel1.setText("DIstrubutor Work Area");
 
@@ -108,8 +108,8 @@ public class DistributionWorkAreaJPanel extends javax.swing.JPanel {
                     .addGroup(layout.createSequentialGroup()
                         .addGap(64, 64, 64)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(VaccinationjDateChooser, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel2))
+                            .addComponent(jLabel2)
+                            .addComponent(VaccinationjDateChooser, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(33, 33, 33)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(btnSchedule, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -129,8 +129,8 @@ public class DistributionWorkAreaJPanel extends javax.swing.JPanel {
                     .addComponent(txtvaccineCount, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(VaccinationjDateChooser, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnSchedule))
+                    .addComponent(btnSchedule)
+                    .addComponent(VaccinationjDateChooser, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(206, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
